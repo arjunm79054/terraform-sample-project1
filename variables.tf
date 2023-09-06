6a0b63b0f836b5295502183758bd01876d7f1d43
Ubuntu <ubuntu@ip-172-31-32-96.ec2.internal>
2023-09-06 08:37:19 +0000

    terraform sample ec2 instance and s3 bucket provisioning project codes

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "ami" {
+	default = "ami-051f7e7f6c2f40dc1"
+}
+variable "instance_type" {
+	default = "t2.micro"
+}
+variable "instance_name" {
+	default = "batch3-demo-instance"
+}
+variable "bucket_name" {
+	default = "batch3-demo-bucket-arjun-demo-batch"
+}
